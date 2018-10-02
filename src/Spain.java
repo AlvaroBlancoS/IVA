998865bd1215244b4c83b658c1075ad99162dda8
ablanco <alvaroblanconsangines.to@gmail.com>
2018-10-02 20:10:01 +0200

    Como crear el IVA

---
+++ src/Spain.java
@@ -0,0 +1,17 @@
+
+public class Spain {
+
+	public static void main(String[] args) {
+		// El IVA 
+		final double IVA  = 1.19;
+	//Cantidad de un producto
+		int cantidad = 25; 
+		
+		System.out.println("En total con IVA es;" +" "+ (cantidad * IVA));
+	}
+    
+	//public double calcular(double IVA, double cantidad) {
+      //  return (IVA / 100) * cantidad;
+    //} 	
+
+}
